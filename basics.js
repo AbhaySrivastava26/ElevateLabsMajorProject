84d88a4336630d21e3ee356948bb339b71ba6644
AbhaySrivastava26 <abhaysrivastava2627@gmail.com>
2025-05-24 00:49:28 +0530

    com

---
+++ basics.js
@@ -0,0 +1,33 @@
+
+$jfjfjf=123232222222
+//threee variables let ,const and var
+
+let a=1
+// we wont be using var anyday
+// we will be using let and const
+// const PI=3.14
+// console.log(a);
+// console.log(PI)
+// {
+//     let a=3
+//     console.log(a)
+// }
+const student ={
+    name:"Abhay",
+    isPass :true,
+    cgpa:8.43,
+
+
+};
+student['cgpa']=student['cgpa']+1;
+// console.log(student.cgpa)
+
+//data types in javascript 
+
+let x = 5;
+let y = 10;
+let sum = x + y;
+document.write(sum)
+
+console.log(sum)
+alert("ABhay is herer")
