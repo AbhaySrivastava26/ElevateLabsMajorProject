b6ccab78ec5134afa7e1e6ffde5a715428ec02b6
AbhaySrivastava26 <abhaysrivastava2627@gmail.com>
2025-10-26 01:06:04 +0530

    node

---
+++ jsseriesapnacollege/node/math.js
@@ -0,0 +1,13 @@
+// export const add=(a,b)=>a+b;
+// export const subtract=(a,b)=>a-b;
+// export const multiply=(a,b)=>a*b;
+// export const divide=(a,b)=>a/b;
+   
+
+
+
+
+import { generate} from "random-words";
+
+console.log(generate(5));
+console.log(generate(10,{ minLength: 5, maxLength: 5 }));
---
+++ jsseriesapnacollege/node/script.js
@@ -0,0 +1,8 @@
+// const math=require('./math');
+// console.log("Addition is: "+math.add(5,3));
+// console.log("Subtraction is: "+math.subtract(5,3));
+// console.log("Multiplication is: "+math.multiply(5,3));
+// console.log("Division is: "+math.divide(6,3));
+import {multiply,divide} from "./math.js"
+console.log(multiply(4,8))
+console.log(divide(4,8))
